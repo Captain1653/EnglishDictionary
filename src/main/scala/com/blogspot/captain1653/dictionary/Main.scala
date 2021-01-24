5f1acfa38f802d9f4c9a305c26ed78092e8241ed
Andrey Orlov <aorlov@stiltsoft.com>
2021-01-24 21:49:19 +0300

    Сохраняем по-простому стастистику за сессию в текстовый файл

--- src/main/scala/com/blogspot/captain1653/dictionary/Main.scala
+++ src/main/scala/com/blogspot/captain1653/dictionary/Main.scala
@@ -7,6 +7,10 @@ import com.blogspot.captain1653.dictionary.questionstrategy.QuestionStrategyFact
 import com.blogspot.captain1653.dictionary.wordsreader.TextFileWordsReader
 import com.blogspot.captain1653.dictionary.wordsstream.WordsStreamFactory
 
+import java.io.{BufferedWriter, File, FileWriter}
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
 object Main {
 
   def main(args: Array[String]): Unit = {
@@ -28,6 +32,19 @@ object Main {
     println(s"Count question is: ${sessionResult.countQuestions}")
     println(s"You have done ${sessionResult.mistakes.size} mistakes")
     if (sessionResult.mistakes.nonEmpty) println(s"Words are: ${sessionResult.mistakes.mkString(",  ")}")
+
+    val fileName = new SimpleDateFormat("dd-MM-yyyy").format(Calendar.getInstance().getTime()) + ".txt"
+    val filePath = "/home/andrey/Others/english/sessions/" + fileName;
+    writeFile(filePath,sessionResult.mistakes)
+  }
+
+  def writeFile(filename: String, lines: Set[String]): Unit = {
+    val file = new File(filename)
+    val bw = new BufferedWriter(new FileWriter(file))
+    for (line <- lines) {
+      bw.write(line + "\n")
+    }
+    bw.close()
   }
 
 }
