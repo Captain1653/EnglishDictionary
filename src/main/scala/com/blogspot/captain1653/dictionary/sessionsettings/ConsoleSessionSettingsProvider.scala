1050d4af9acaddde8ea18cf527c64ceafe0bae8f
Andrey Orlov <aorlov@stiltsoft.com>
2021-02-13 23:23:36 +0300

    Добавил цифры для ввода условий выполнения упражнений

--- src/main/scala/com/blogspot/captain1653/dictionary/sessionsettings/ConsoleSessionSettingsProvider.scala
+++ src/main/scala/com/blogspot/captain1653/dictionary/sessionsettings/ConsoleSessionSettingsProvider.scala
@@ -14,14 +14,14 @@ class ConsoleSessionSettingsProvider extends SessionSettingsProvider {
     val filePaths = StdIn.readLine().split(",")
     val wordsReader = new TextFileWordsReader(filePaths)
 
-    println("Words order: seq, rand")
+    println("Words order: 1 - seq, 2 - rand")
     val wordsOrder = parseWordsOrder(StdIn.readLine())
 
-    println("Input type of words: noun, adjective, adverb, verb, all")
+    println("Input type of words: 1 - noun, 2 - adjective, 3 - adverb, 4 - verb, 5 - all")
     val wordTypePredicate = WordsTypePredicate(parseWordType(StdIn.readLine()))
     val words = wordsReader.getWords(wordTypePredicate)
 
-    println("Question Strategy: en, ru, mix")
+    println("Question Strategy: 1 - en, 2 - ru, 3 - mix")
     val questionStrategyType = parseQuestionStrategyType(StdIn.readLine())
 
     new SessionSettings {
@@ -32,21 +32,22 @@ class ConsoleSessionSettingsProvider extends SessionSettingsProvider {
   }
 
   private def parseWordsOrder(wordsOrder: String): WordsOrder.Value = wordsOrder match {
-    case "seq" => WordsOrder.SEQUENTIAL
-    case "rand" => WordsOrder.RANDOM
+    case "1" => WordsOrder.SEQUENTIAL
+    case "2" => WordsOrder.RANDOM
   }
 
   private def parseQuestionStrategyType(questionStrategy: String): QuestionStrategyType.Value = questionStrategy match {
-    case "ru" => QuestionStrategyType.RUSSIAN
-    case "en" => QuestionStrategyType.ENGLISH
-    case "mix" => QuestionStrategyType.MIX
+    case "1" => QuestionStrategyType.ENGLISH
+    case "2" => QuestionStrategyType.RUSSIAN
+    case "3" => QuestionStrategyType.MIX
   }
 
   private def parseWordType(wordType: String): WordSearchCriteria = wordType match {
-    case "noun" => WordSearchCriteria("noun", WordType.NOUN)
-    case "verb" => WordSearchCriteria("verb", WordType.VERB)
-    case "adjective" => WordSearchCriteria("adjective", WordType.ADJECTIVE)
-    case "adverb" => WordSearchCriteria("adverb", WordType.ADVERB)
+    case "1" => WordSearchCriteria("noun", WordType.NOUN)
+    case "2" => WordSearchCriteria("verb", WordType.ADJECTIVE)
+    case "3" => WordSearchCriteria("adjective", WordType.ADVERB)
+    case "4" => WordSearchCriteria("adverb", WordType.VERB)
+    //case "5" => WordSearchCriteria("adverb", WordType.ALL)
   }
 
 }
